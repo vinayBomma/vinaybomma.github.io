1a36d9d7e7e72a003be330693e04a29a1de5601e
vinaybomma <vinaysb2000@gmail.com>
2025-09-04 11:48:06 +0530

    Removed about section

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
-import About from "./sections/About";
 import Skills from "./sections/Skills";
 import Projects from "./sections/Projects";
 import Contact from "./sections/Contact";
@@ -16,7 +15,6 @@ function App() {
         <Header />
         <main>
           <Hero />
-          <About />
           <Skills />
           <Projects />
           <Contact />
